96c2a4fc96a42fe5cb85cfe98708892ae5d037a6
Saad Salman <saadsalman@MacBook.local>
2023-03-07 14:15:33 +0300

    Update

--- functions/functions.js
+++ functions/functions.js
@@ -1,6 +1,6 @@
 export function backendURL () {
     // return 'http://127.0.0.1:8000'
-    return 'https://2257-31-166-34-44.in.ngrok.io'
+    return 'https://fa77-31-166-34-44.in.ngrok.io'
 }
 
 export function graphQLUrl (image) {
